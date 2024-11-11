5835aea49f62fd1819c86e88291ea3aaa77ba558
cheryx <roxmb25@gmail.com>
2024-11-11 20:21:38 +0100

    Refactor project initialization and update package version to 1.0.4

--- bin/kochanowski.js
+++ bin/kochanowski.js
@@ -2,9 +2,11 @@
 
 import fs from "fs";
 import path from "path";
-import { execSync } from "child_process";
+import { exec } from "child_process";
 import inquirer from "inquirer";
 import chalk from "chalk";
+
+const HELLO_WORLD_FILE = "indeks.kpl";
 
 async function createKochanowskiProject() {
   const questions = [
@@ -35,30 +37,22 @@ async function createKochanowskiProject() {
   console.log(
     chalk.blue(`Initializing a new Kochanowski project in ${projectPath}`)
   );
-  execSync("npm init -y ", { stdio: [] });
+  exec("npm init -y", { stdio: 'inherit' }, (error) => {
+    if (error) {
+      console.error(chalk.red("Failed to initialize npm project"), error);
+      process.exit(1);
+    }
 
-  console.log(chalk.blue("Installing Kochanowski package..."));
-  execSync("npm install --silent kochanowski", { stdio: "inherit" });
-
-  createHelloWorldFile();
-  addStartScript();
-
-  console.log(chalk.green("Kochanowski project created successfully!"));
-}
-
-function addStartScript() {
-  const packageJsonPath = path.join(process.cwd(), "package.json");
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
-
-  packageJson.scripts = packageJson.scripts || {};
-  packageJson.scripts.start = "kochanowski indeks.kpl";
-
-  fs.writeFileSync(
-    packageJsonPath,
-    JSON.stringify(packageJson, null, 2),
-    "utf8"
-  );
-  console.log(chalk.green("Added start script to package.json."));
+    console.log(chalk.blue("Installing Kochanowski package..."));
+    exec("npm install --silent kochanowski", { stdio: "inherit" }, (error) => {
+      if (error) {
+        console.error(chalk.red("Failed to install Kochanowski package"), error);
+        process.exit(1);
+      }
+      addStartScript();
+      createHelloWorldFile();
+    });
+  });
 }
 
 function createHelloWorldFile() {
@@ -77,6 +71,28 @@ Drukuj druga liczba.
 Przejdź do następnej iteracji.`;
   fs.writeFileSync("indeks.kpl", helloWorldContent, "utf8");
   console.log(chalk.green("Created indeks.kpl with sample content."));
+}
+
+/**
+ * Adds a start script to the package.json file.
+ */
+function addStartScript() {
+  const packageJsonPath = getPackageJsonPath();
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+
+  packageJson.scripts = packageJson.scripts ?? {};
+  packageJson.scripts.start = `kochanowski ${HELLO_WORLD_FILE}`;
+
+  fs.writeFileSync(
+    packageJsonPath,
+    JSON.stringify(packageJson, null, 2),
+    "utf8"
+  );
+  console.log(chalk.green("Added start script to package.json."));
+}
+
+function getPackageJsonPath() {
+  return path.join(process.cwd(), "package.json");
 }
 
 createKochanowskiProject().catch((error) => {
